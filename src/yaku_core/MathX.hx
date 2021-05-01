8068b26a7a61032c8967e7e535c959285f7de717
scott <scott@scottplusplus.com>
2021-05-01 18:08:53 -0400

    adding clamp

--- src/yaku_core/MathX.hx
+++ src/yaku_core/MathX.hx
@@ -2,6 +2,15 @@ package yaku_core;
 
 class MathX {
 	public static function map(val:Float, inFrom:Float, inTo:Float, outFrom:Float, outTo:Float):Float {
-        return outFrom + (val - inFrom) * (outTo - outFrom) / (inTo - inFrom);
+		return outFrom + (val - inFrom) * (outTo - outFrom) / (inTo - inFrom);
+	}
+
+	public static function clamp(value:Float, min:Float, max:Float):Float {
+		if (value < min)
+			return min;
+		else if (value > max)
+			return max;
+		else
+			return value;
 	}
 }
