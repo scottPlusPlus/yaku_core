7bd5aeef4e3ba7673fdd34b0c01abe22e2f722ba
scott <scott@scottplusplus.com>
2021-05-05 19:04:46 -0400

    adding MathX

--- src/yaku_core/MathX.hx
+++ src/yaku_core/MathX.hx
@@ -13,4 +13,42 @@ class MathX {
 		else
 			return value;
 	}
+
+	// https://stackoverflow.com/questions/23689001/how-to-reliably-format-a-floating-point-number-to-a-specified-number-of-decimal
+	public static function toStringPrecision(x:Float, precision:UInt):String {
+		if (x == 0){
+			return "0." + ([for (i in 0...precision) "0"].join("")); // quick return
+		}
+
+		var negative:Bool = (x < 0.0);
+		x = Math.abs(x);
+		var intPart:Int = Math.floor(x);
+		var p = Math.pow(10, precision);
+		var fracPart = Math.round(p * (x - intPart));
+		var buf:StringBuf = new StringBuf();
+
+		if (negative) {
+			buf.addChar("-".code);
+		}
+			
+		buf.add(Std.string(intPart));
+		buf.addChar(".".code);
+		if (fracPart == 0) {
+			for (_ in 0...precision)
+				buf.addChar("0".code);
+		} else {
+			p = p / 10;
+			while (fracPart < p) {
+				p = p / 10;
+				buf.addChar("0".code);
+			}
+			buf.add(fracPart);
+		}
+		return buf.toString();
+	}
+
+	public static function truncate(x:Float, precision:UInt):Float {
+		var str = toStringPrecision(x, precision);
+		return Std.parseFloat(str);
+	}
 }
--- test/yaku_core_test/MathXTest.hx
+++ test/yaku_core_test/MathXTest.hx
@@ -4,18 +4,41 @@ import yaku_core.MathX;
 import utest.Assert;
 
 class MathXTest extends utest.Test {
-    
-    var x = 0.0;
+	var x = 0.0;
 
-    function test_map() {
-        x = MathX.map(50, 0, 100, 0, 1000);
-        Assert.equals(500, x);
+	function test_map() {
+		x = MathX.map(50, 0, 100, 0, 1000);
+		Assert.equals(500, x);
 
-        x = MathX.map(10, 0, 100, 0, 1000);
-        Assert.equals(100, x);
+		x = MathX.map(10, 0, 100, 0, 1000);
+		Assert.equals(100, x);
 
-        x = MathX.map(500, 0, 1000, 0, 100);
-        Assert.equals(50, x);
+		x = MathX.map(500, 0, 1000, 0, 100);
+		Assert.equals(50, x);
+	}
+
+    function test_clamp(){
+        Assert.equals(10, MathX.clamp(12, 0, 10));
+        Assert.equals(0, MathX.clamp(-5, 0, 10));
+        Assert.equals(7, MathX.clamp(7, 0, 10));
     }
 
+	function testTrucate() {
+		var val = 1.2345678;
+		var trunc = function(prec:UInt):Float {
+			return MathX.truncate(val, prec);
+		};
+		Assert.equals(1, trunc(0));
+		Assert.equals(1.23, trunc(2));
+		Assert.equals(1.235, trunc(3));
+        Assert.equals(1.234568, trunc(6));
+
+		val = 1234;
+		Assert.equals(1234, trunc(2));
+		Assert.equals(1234, trunc(3));
+
+		val = -1.2345678;
+		Assert.equals(-1.23, trunc(2));
+		Assert.equals(-1.235, trunc(3));
+	}
 }
