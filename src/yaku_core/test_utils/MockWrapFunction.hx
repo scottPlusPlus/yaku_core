c979572cd0c3f4826aec2ffcbf21fd17aedd1330
scott <scott@scottplusplus.com>
2021-12-25 14:47:45 -0800

    updating MockWrapFunction

--- src/yaku_core/test_utils/MockWrapFunction.hx
+++ src/yaku_core/test_utils/MockWrapFunction.hx
@@ -1,35 +1,58 @@
 package yaku_core.test_utils;
 
+import tink.CoreApi.SignalTrigger;
+import tink.CoreApi.Signal;
 import tink.core.Noise;
 using yaku_core.NullX;
+using  yaku_core.ArrayX;
 
 class MockWrapFunction<IN,OUT> {
 
-    public var history:Array<InOut<IN,OUT>> = [];
     public var implementation:Action<IN,OUT>;
-    public var next:Null<Action<IN,OUT>>;
+    public var scheduledOverrides:Array<Null<Action<IN,OUT>>> = [];
+    public var history:Array<InOut<IN,OUT>> = [];
+    
+    public var signal (get,never):Signal<MockWrapFunction<IN,OUT>>;
+    public function get_signal():Signal<MockWrapFunction<IN,OUT>> {
+        return signalTrigger.asSignal();
+    }
+    private var signalTrigger:SignalTrigger<MockWrapFunction<IN,OUT>>;
+
+    public inline function index():Int {
+        return history.length;
+    }
+
+    public function new(action:Action<IN,OUT>){
+        signalTrigger = new SignalTrigger();
+        implementation = action;
+    }
 
     public function call(input:IN):OUT{
-        var act = next.orFallback(implementation);
-        next = null;
+        signalTrigger.trigger(this);
+        var act = scheduledOverrides.getOrFallback(index(), implementation);
         var out = act.call(input);
         history.push({input:input, output:out});
         return out;
     }
  
-    public function overrideNext(action:Action<IN,OUT>){
-        next = action;
-    }
-
-    public function new(action:Action<IN,OUT>){
-        implementation = action;
+    /*
+        Overrides the next call of the function with the provided action.
+        If nextIndex is provided, will override that call instead
+        (example: 0th and 1st next calls will use standard implementation, but the next one will use the provided override)
+    */
+    public function overrideNextCall(action:Action<IN,OUT>, nextIndex:UInt = 0){
+        nextIndex += index();
+        scheduledOverrides.resize(nextIndex);
+        scheduledOverrides[nextIndex] = action;
     }
 }
+
 
 typedef InOut<IN,OUT> = {
     input:IN,
     output:OUT
 }
+
 
 abstract Action<X,Y>(X->Y) from X->Y to X->Y {
     
--- test/yaku_core_test/test_utils/MockWrapFunctionTest.hx
+++ test/yaku_core_test/test_utils/MockWrapFunctionTest.hx
@@ -1,5 +1,6 @@
 package yaku_core_test.test_utils;
 
+import yaku_core.test_utils.SignalTester;
 import yaku_core.test_utils.MockWrapFunction;
 import yaku_core.test_utils.TestVals;
 import utest.Assert;
@@ -13,6 +14,10 @@ class MockWrapFunctionTest extends utest.Test {
 		return TestVals.foo;
 	}
 
+	private function historyToOutputs(io:InOut<Int,String>):String {
+		return io.output;
+	}
+
 	public function testMocks() {
 		var expectedHistory = [];
 
@@ -20,7 +25,7 @@ class MockWrapFunctionTest extends utest.Test {
 		Assert.equals("1", mock.call(1));
 		expectedHistory.push({input: 1, output: "1"});
 
-		mock.overrideNext(justFoo);
+		mock.overrideNextCall(justFoo);
 		Assert.equals(TestVals.foo, mock.call(1));
 		Assert.equals("1", mock.call(1));
 		expectedHistory.push({input: 1, output: TestVals.foo});
@@ -37,8 +42,29 @@ class MockWrapFunctionTest extends utest.Test {
 
 	public function testOverrideVal() {
 		var mock = new MockWrapFunction(printInt);
-		mock.overrideNext(TestVals.bar);
+		mock.overrideNextCall(TestVals.bar);
 		Assert.equals(TestVals.bar, mock.call(123));
+	}
+
+	public function testMultipleOverrides() {
+		var mock = new MockWrapFunction(printInt);
+		mock.overrideNextCall(TestVals.bar, 1);
+		mock.overrideNextCall(TestVals.bar2, 2);
+		mock.call(0);
+		mock.call(1);
+		mock.call(2);
+		mock.call(3);
+		var expectedHistory = ["0", TestVals.bar, TestVals.bar2, "3"];
+		var actual = mock.history.map(historyToOutputs);
+		Assert.same(expectedHistory, actual);
+	}
+
+	public function testSignal() {
+		var mock = new MockWrapFunction(printInt);
+		var signalTester = new SignalTester(mock.signal);
+		mock.call(0);
+		mock.call(1);
+		Assert.equals(2, signalTester.input.length);
 	}
 
 	public function testVoidOut() {
@@ -60,4 +86,5 @@ class MockWrapFunctionTest extends utest.Test {
 		var val = mock.call(null);
 		Assert.equals(5, val);
 	}
+
 }
