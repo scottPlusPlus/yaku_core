a1072fc39aa63a1ba9f4ff181b5755c2f7613214
scott <scott@scottplusplus.com>
2021-12-28 03:38:40 -0800

    adding ArrayX.isSame

--- src/yaku_core/ArrayX.hx
+++ src/yaku_core/ArrayX.hx
@@ -15,5 +15,28 @@ class ArrayX {
     public static inline function getOrFallback<T>(arr:Array<T>, index:UInt, fallback:Lazy<T>):T {
         return getOrNull(arr, index).orFallback(fallback);
     }
+
+    public static function isSame<T>(a1:Array<T>, a2:Array<T>, ?equality:T->T->Bool):Bool {
+        if (a1 == null && a2 == null){
+            return true;
+        }
+        if (a1 == null || a2 == null){
+            return false;
+        }
+        if (a1.length != a2.length){
+            return false;
+        }
+        if (equality == null){
+            equality = function(t1, t2){
+                return t1 == t2;
+            };
+        }
+        for (index in 0...a1.length){
+            if (!equality(a1[index], a2[index])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
--- test/yaku_core_test/ArrayXTest.hx
+++ test/yaku_core_test/ArrayXTest.hx
@@ -21,4 +21,25 @@ class ArrayXTest extends utest.Test {
         var actual = ArrayX.getOrNull(array, 3);
         Assert.same(null, actual);
     }
+
+    public function testIsSameStrings(){
+        var a1 = ["a", "b", "c"];
+
+        Assert.isTrue(a1.isSame(["a", "b", "c"]));
+        Assert.isFalse(a1.isSame(["a", "b"]));
+        Assert.isFalse(a1.isSame(["a", "b", "x"]));
+        Assert.isFalse(a1.isSame(null));
+
+        var sameStrings = function(s1:String, s2:String):Bool {
+            return s1.length == s2.length;
+        }
+        Assert.isTrue(a1.isSame(["x", "y", "z"], sameStrings));
+    }
+
+    public function testIsSameInts(){
+        var a1 = [1,2,3];
+        Assert.isTrue(a1.isSame([1,2,3]));
+        Assert.isFalse(a1.isSame([1,2,99]));
+        Assert.isFalse(a1.isSame([1]));
+    }
 }
